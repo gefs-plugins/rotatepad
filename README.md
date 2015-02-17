169c6404fd6bd568dcbd0716831fcd382c6b518f
Karl Cheng <qantas94heavy@gmail.com>
2015-02-17 10:06:57 +0000

    Initial commit
    This is the first commit of RotatePad, a browser extension which fixes the broken joystick functionality in GEFS-Online, the free flight simulator based on the Google Earth Plugin (http://www.gefs-online.com).

---
+++ README.md
@@ -0,0 +1,25 @@
+RotatePad
+=========
+
+RotatePad is an unofficial extension for GEFS-Online
+(http://www.gefs-online.com), the free flight simulator based on the Google
+Earth Plugin.  Though GEFS provides some functionality to use your joystick,
+it is unfortunately broken and doesn't work in most browsers.
+
+This extension allows users of Chrome, Firefox and Opera 15+ to use the native
+browser functionality as controls for GEFS.  Support for Internet Explorer and
+Safari is a work in progress due to a lack of native support on the platform.
+
+We recommend using Google Earth Plugin v6.2.2.6613, as later versions use a
+new terrain data model which makes GEFS unusuable.
+
+If you're interested in asking and answering question about aviation, come
+and join [Aviation Stack Exchange][1]!
+
+How to contribute
+-----------------
+
+If you'd like to contribute to this project, follow the instructions given
+in the file `CONTRIBUTING.md`.
+
+  [1]: http://aviation.stackexchange.com
---
+++ rotatepad_v0.1.0.user.js
@@ -0,0 +1,78 @@
+// ==UserScript==
+// @name RotatePad for GEFS-Online
+// @description This extension (by Qantas 94 Heavy) allows you to use your joystick in GEFS.
+// @namespace GEFS-Plugins
+// @match http://www.gefs-online.com/gefs.php*
+// @match http://gefs-online.com/gefs.php*
+// @run-at document-end
+// @version 0.1.0
+// @grant none
+// ==/UserScript==
+
+(function (fn) {
+  // check if ges.init has already been called
+  if (window.ges && ges.map3d) fn();
+  else {
+    var oldInit = ges.init;
+    var timer = setInterval(function () {
+      if (window.ges && ges.init) {
+        clearInterval(timer);
+        ges.init = function () {
+          fn();
+          oldInit();
+        };
+      }
+    }, 4);
+  }
+})(function () {
+  controls.joystick.test = function() {
+    controls.joystick.api = null;
+    if (navigator.getGamepads && navigator.getGamepads()[0]) {
+      controls.joystick.api = 'html5';
+      controls.joystick.stick = navigator.getGamepads()[0];
+      controls.joystick.info = '<b>' + controls.joystick.stick.id + '</b> detected on native HTML5 API';
+    } else {
+      controls.joystick.stick = new Joystick();
+      if (controls.joystick.stick.isConnected()) {
+        controls.joystick.api = 'extension';
+        controls.joystick.info = '<b>Controller</b> detected on JS Joystick extension';
+      }
+    }
+    return controls.joystick.api !== null;
+  };
+
+  controls.joystick.getAxis = function(axis) {
+    if (controls.joystick.api === 'html5') {
+      var ret;
+      switch (axis) {
+        case 'x':
+          ret = controls.joystick.stick.axes[0];
+          break;
+        case 'y':
+          ret = controls.joystick.stick.axes[1];
+          break;
+        case 'z':
+          ret = controls.joystick.stick.axes[2];
+          break;
+        case 'r':
+          ret = controls.joystick.stick.axes[3];
+          break;
+      }
+
+      if (ret !== undefined) return Math.floor(ret * 32767.5 + 32767.5);
+    }
+    else switch (axis) {
+      case 'x': return controls.joystick.stick.getX();
+      case 'y': return controls.joystick.stick.getY();
+      case 'z': return controls.joystick.stick.getZ();
+      case 'r': return controls.joystick.stick.getR();
+    }
+  };
+
+  controls.joystick.checkButton = function(button) {
+    if (controls.joystick.api === 'html5') return controls.joystick.stick.buttons[button - 1].pressed;
+
+    var value = controls.joystick.stick.getButtons();
+    return value & (1 << (button - 1)) !== 0;
+  };
+});
